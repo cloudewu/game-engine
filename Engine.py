6d37d3343c77defa756dc04e00b2a7ca703df03c
joanna <joannawu@nlplab.cc>
2021-11-04 19:33:08 +0800

    add movement subscription

--- Engine.py
+++ Engine.py
@@ -1,52 +1,78 @@
 from operator import itemgetter
 from collections import defaultdict
+from typing import Callable, Tuple
 from pynput import keyboard
-from typing import Callable
 
 class Engine(object):
     KB_EVENT = ['press', 'release']
-    VR_KEYS = ['']
+    EVENT = []
+    CONTROL_KEY = {
+        keyboard.Key.up: 'up', 
+        keyboard.Key.down: 'down', 
+        keyboard.Key.right: 'right', 
+        keyboard.Key.left: 'left'
+    }
 
-    def __init__(self, width, height, debug=False) -> None:
+    def __init__(self, width, height, move, debug=False) -> None:
         super().__init__()
+
         self.width = width
         self.height = height
+        self.move_cb = move
         self.debug = debug
+
+        self.character = [int(width/2), int(height/2)]
+        self.map = [['*'] * width] * height
+        self.isend = False
 
         self._timestamp = 0
         self._kb_callback = {e: defaultdict(list) for e in self.KB_EVENT}
-        self.map = [['*'] * width] * height
-        self.isend = False
-    
+        self._subscription = {e: [] for e in self.EVENT}
+
     def start(self) -> bool:
         while not self.isend:
             self.render_map()
             self._listen()
-    
+
     def end(self) -> None:
-        print('end')
         self.isend = True
-        self._end()
+        self._cleanup()
         return True
+
+    # Movement
+    def position(self, x: int = None, y: int = None) -> Tuple[int,int]:
+        if x is not None: self.character[0] = x
+        if y is not None: self.character[1] = y
+        return self.character[:]
+
+    def move(self, direction) -> None:
+        x, y = self.move_cb(direction, *self.position())
+        self.position(x, y)
+        self.log(f'move to {x}, {y}')
 
     # Map
     def render_map(self) -> None:
         print()
         print(f'time: {self._timestamp:3}')
         print('.', '-' * self.width, '.', sep='')
-        for i in range(self.height):
+        for j in range(self.height):
             print('|', end='')
-            for j in range(self.width):
-                print('.', end='')
+            for i in range(self.width):
+                if i == self.character[0] and j == self.character[1]:
+                    print('x', end='')
+                else:
+                    print('.', end='')
             print('|')
         print('\'', '-' * self.width, '\'', sep='')
         return True
 
     def update_map(self) -> bool:
+        # [ TODO ]
         ...
 
     # Subscriber
     def add_event(self) -> bool:
+        # [ TODO ]
         ...
     
     def subscribe_keyboard(self, key: str, action: str, callback: Callable) -> bool:
@@ -79,14 +105,34 @@ class Engine(object):
         self._kb_callback[action][key].remove(callback)
         return True
     
-    def subscribe(self) -> bool:
+    def subscribe(self, event: str, callback: Callable) -> bool:
+        """ Subscribe to a global event.
+        Available event: 'movement'
+        """
+        # if event not in self.EVENT: 
+        #     self.log(f'action "{event}" not allowed. Callback not subscribed', 'warn')
+        #     self.log(f'Available actions: {self.EVENT}', 'warn')
+        #     return False
+        
+        # self._subscription[event].append(callback)
+        # return True
+        ...
+    
+    def unsubscribe(self, event: str, callback: Callable) -> bool:
+        """ Unsubscribe the first occurence of given callback """
+        # if event not in self.EVENT: 
+        #     self.log(f'event "{event}" not found', 'warn')
+        #     return False
+        # if callback not in self._kb_callback[event]:
+        #     self.log(f'callback {callback.__name__} not found', 'warn')
+        #     return False
+
+        # self._subscription[event].remove(callback)
+        # return True
         ...
     
     def timer(self) -> bool:
-        ...
-    
-    # Movement
-    def move() -> None:
+        # [ TODO ]
         ...
     
     # utilities
@@ -94,7 +140,7 @@ class Engine(object):
         self._timestamp += 1
         return self._timestamp
     
-    def _end(self) -> bool:
+    def _cleanup(self) -> bool:
         print('\n - end - \n')
         return True
     
@@ -106,6 +152,9 @@ class Engine(object):
 
     def _handle_keyboard(self, event) -> None:
         action = type(event).__name__.lower()
+
+        if action == 'press' and event.key in self.CONTROL_KEY:
+            self.move(self.CONTROL_KEY[event.key])
         for cb in self._kb_callback[action][event.key]:
             cb()
 
--- Game.py
+++ Game.py
@@ -1,9 +1,23 @@
 from Engine import Engine
 
-map = [[]]
+WIDTH, HEIGHT = 40, 10
+
+def move(action, x, y):
+    if action == 'up':
+        ...
+    elif action == 'down':
+        ...
+    elif action == 'left':
+        ...
+    elif action == 'right':
+        ...
+    return [x, y]
 
 # SETUP
-Game = Engine(40, 10, debug=True)
+Game = Engine(WIDTH, HEIGHT,  # The map size of your game
+              move, # The movement controller you write
+              debug=True
+             )
 
 Game.subscribe_keyboard('q', 'press', lambda: print('press q'))
 Game.subscribe_keyboard('esc', 'press', Game.end)
