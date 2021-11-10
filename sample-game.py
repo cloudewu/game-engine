2d09d386eb58798e94d899289bf61d431819def1
joanna <joannawu@nlplab.cc>
2021-11-11 01:44:54 +0800

    improve sample code

--- sample-game.py
+++ sample-game.py
@@ -1,7 +1,6 @@
-from Game.Engine import Engine
+from Game.core import Engine
 
-WIDTH, HEIGHT = 40, 10
-
+# Kind reminder to TA: Don't release your code on web ;D
 def move(action, x, y):
     if action == 'up':
         ...
@@ -13,14 +12,7 @@ def move(action, x, y):
         ...
     return [x, y]
 
-def backpack_renderer(game: Engine):
-    print()
-    print('---------------------------------')
-    print('You are in your backpack!')
-    print(game.backpack) # access all properties in game
-    print('---------------------------------')
-    print()
-
+# DEMO: custom map renderer (set in Engine initialization)
 def map_renderer(game):
     print()
     print(f'time: {game._timestamp:3}')
@@ -35,24 +27,23 @@ def map_renderer(game):
 
 
 # SETUP
-game = Engine(WIDTH, HEIGHT,  # The map size of your game
-              move, # The movement controller
-              # map_renderer, # write your own map renderer (use this only when you are confident about what you are doing)
+WIDTH, HEIGHT = 40, 10
+game = Engine(WIDTH, HEIGHT,     # The map size of your game
+              move,              # The movement controller
+              # input = 'stdin', # forcefully set the input system. 
+                                 # Available options: [stdin, pynput (only if supported)]
+              # map_renderer,    # write your own map renderer 
+                                 # (use this only when you are confident about what you are doing)
               debug = True
              )
 
 # DEMO: custom layer
+def backpack_renderer(game: Engine):
+    print('\n---------------------------------')
+    print('You are in your backpack!')
+    print(game.backpack) # small showcase: you can access all properties stored in the engine
+    print('---------------------------------\n')
 game.add_layer('backpack', backpack_renderer)
-
-# DEMO: remove item by location
-item = game.add_item('star', 3, 20, '*')
-item.add_event('removed', lambda i: print('*** (1) I am removed qq ***'))
-game.remove_item(x = item.x, y = item.y)
-
-# DEMO: remove item by name
-item = game.add_item('star', 3, 20, '*')
-item.add_event('removed', lambda i: print('*** (2) I am removed qq ***'))
-game.remove_item(name = item.name)
 
 # DEMO: item
 item = game.add_item('dollar',       # item name
@@ -63,9 +54,24 @@ item = game.add_item('dollar',       # item name
                      life = 10       # how many rounds should the item be automatically removed
                      )
 item.timer(5, lambda i: i.show(True)) # show the item after 5 rounds
-item.add_event('removed', lambda i: print('*** (3) I am removed qq ***'))
+item.add_event('removed', lambda i: print('*** (1) I am removed qq ***'))
 item.add_event('enter', lambda i: print('*** You found me! ***'))
 item.add_event('leave', lambda i: print('*** You leaved me qwq *** '))
+
+# DEMO: remove item by location
+item = game.add_item('star', 3, 20, '*')
+item.add_event('removed', lambda i: print('*** (2) I am removed qq ***'))
+game.remove_item(x = item.x, y = item.y)
+
+# DEMO: remove item by name
+item = game.add_item('star', 3, 20, '*')
+item.add_event('removed', lambda i: print('*** (3) I am removed qq ***'))
+game.remove_item(name = item.name)
+
+# DEMO: remove item by location and name (useful when you are lazy checking the current tile)
+item = game.add_item('star', 3, 20, '*')
+item.add_event('removed', lambda i: print('*** (4) I am removed qq ***'))
+game.remove_item(3, 30, name = 'not-star') # you should see that the item is not removed
 
 # DEMO: default events
 game.subscribe('onstart', lambda game: print('*** Game start ***'))
@@ -87,5 +93,6 @@ game.unsubscribe_keyboard('t', 'press', test)
 # MAIN
 for day in game.start():
     if day == None: break
+    # do something between steps here (add timers, subscriptions, events...)
     print(f' *** Day {day} ends *** ')
     
